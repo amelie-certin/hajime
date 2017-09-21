6ea3995bbb9764f3feab2c5977d988c6aed8c1ff
Amelie Certin <amelie.certin@gmail.com>
2017-09-21 14:48:37 +0200

    Home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'enter_arena/index'
+  root 'enter_arena#index'
+
+  resources :character, only: %i(new create show)
+  resources :armory, only: %i(index) do
+    collection do
+      get 'unequip'
+      post 'equip'
+    end
+  end
 end
